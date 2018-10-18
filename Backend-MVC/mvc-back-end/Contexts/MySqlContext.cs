f6c1071d137ba4db5d2dc9ea81ed70d7c2a94352
chippy <chippy@chippys-MacBook-Air.local>
2018-10-18 10:27:04 -0500

    MVC Backend

---
+++ Backend-MVC/mvc-back-end/Contexts/MySqlContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using mvcbackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace mvcbackend.Contexts
+{
+    public class MySqlContext :DbContext
+    {
+        public MySqlContext(DbContextOptions<MySqlContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<mvcbackend.Models.Collection> Collections { get; set; }
+    }
+
+}
---
+++ Backend-MVC/mvc-back-end/Models/Collection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace mvcbackend.Models
+{
+    public class Collection
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Collected { get; set; }
+        public int Total { get; set; }
+        public decimal Price { get; set; }
+    }
+}
